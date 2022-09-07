341bbcf28bd473f165aa8c72c5cfb8d0d5ab3dff
aamircodes <aamirp1@outlook.com>
2022-09-07 22:22:43 +0100

    Add frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,52 @@
 import { BrowserRouter, NavLink, Routes, Route } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Home from './Home';
 import Login from './Login';
 import Register from './Register';
 import PremiumContent from './PremiumContent';
+import PublicRoute from './routes/PublicRoute';
+import PrivateRoute from './routes/PrivateRoute';
+import { getUser, getToken, setUserSession, resetUserSession } from './service/AuthService';
+
+const verifyTokenAPIURL = 'https://2d4t6gs6j0.execute-api.us-east-1.amazonaws.com/prod/verify';
 
 function App() {
+  const [isAuthenticating, setAuthenticating] = useState(true);
+
+  useEffect(() => {
+    const token = getToken();
+    if (token === 'undefined' || token === undefined || token === null || !token) {
+      return;
+    }
+
+    const requestConfig = {
+      headers: {
+        'x-api-key': 'tjiIny8rS31SEZPhciRVO3E49rB1gTZs9KNZKmSI',
+      },
+    };
+    const requestBody = {
+      user: getUser(),
+      token: token,
+    };
+
+    axios
+      .post(verifyTokenAPIURL, requestBody, requestConfig)
+      .then((response) => {
+        setUserSession(response.data.user, response.data.token);
+        setAuthenticating(false);
+      })
+      .catch(() => {
+        resetUserSession();
+        setAuthenticating(false);
+      });
+  }, []);
+
+  const token = getToken();
+  if (isAuthenticating && token) {
+    return <div className='content'>Authenicating...</div>;
+  }
+
   return (
     <div className='App'>
       <BrowserRouter>
@@ -17,9 +59,31 @@ function App() {
         <div className='content'>
           <Routes>
             <Route exact path='/' element={<Home />} />
-            <Route exact path='/register' element={<Register />} />
-            <Route exact path='/login' element={<Login />} />
-            <Route exact path='/premium-content' element={<PremiumContent />} />
+            <Route
+              path='/register'
+              element={
+                <PublicRoute>
+                  <Register />
+                </PublicRoute>
+              }
+            />
+            <Route
+              exact
+              path='/login'
+              element={
+                <PublicRoute>
+                  <Login />
+                </PublicRoute>
+              }
+            />
+            <Route
+              path='/premium-content'
+              element={
+                <PrivateRoute>
+                  <PremiumContent />
+                </PrivateRoute>
+              }
+            />
           </Routes>
         </div>
       </BrowserRouter>
--- frontend/src/PremiumContent.js
+++ frontend/src/PremiumContent.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getUser, resetUserSession } from './service/AuthService';
 
 const PremiumContent = () => {
-  return <div>This is the PremiumContent page!</div>;
+  const user = getUser();
+  const name = user !== 'undefined' && user ? user.name : '';
+
+  const navigate = useNavigate();
+
+  const logoutHandler = () => {
+    resetUserSession();
+    navigate('/login');
+  };
+
+  return (
+    <div>
+      Hello {name}! You have logged in! This is the premium content page! <br />
+      <input type='button' value='Logout' onClick={logoutHandler} />
+    </div>
+  );
 };
 
 export default PremiumContent;
